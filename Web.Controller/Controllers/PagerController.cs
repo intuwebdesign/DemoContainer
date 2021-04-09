5f94b4da982d8024c1d0740ebad2046d0cef97e3
George <gwphillipson@yahoo.co.uk>
2021-04-09 12:44:29 +0100

    C# pagination example 09 Apr 2021

---
+++ Web.Controller/Controllers/PagerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Web.Controller.Helpers;
+
+namespace Web.Controller.Controllers
+{
+    public class PagerController : System.Web.Mvc.Controller
+    {
+        [HttpGet]
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SearchResults(string q, int? page = 0)
+        {
+            if (string.IsNullOrEmpty(q))
+            {
+                q = "";
+            }
+            List<ListBeveragesViewModel> model = new List<ListBeveragesViewModel>();
+
+            var beverages = ListOfBeverages.TopAlcoholicDrinksUk();
+
+            foreach (var beverage in beverages.Where(x=>string.Equals(x.Type, q, StringComparison.CurrentCultureIgnoreCase)))
+            {
+                string beverageName = beverage.Beverage;
+                string beverageType = beverage.Type;
+
+                model.Add(new ListBeveragesViewModel(beverageName,beverageType));
+            }
+
+            int totalNumberOfRecords = model.Count;
+
+            int numberOfRecordsPerPage = 5;
+            var numberOfRecordsToDisplay = new PagerHelper.Pagination(totalNumberOfRecords, page, numberOfRecordsPerPage);
+
+            var displayResults = new DisplayPagerViewModel
+            {
+                DisplaySearchResults    = model.Skip((numberOfRecordsToDisplay.CurrentPage - 1)* numberOfRecordsToDisplay.PageSize).Take(numberOfRecordsToDisplay.PageSize).ToList(),
+                Pager                   = numberOfRecordsToDisplay,
+                NumberOfResults         = model.Count
+            };
+            return View("~/Views/Pager/SearchResults.cshtml",displayResults);
+        }
+    }
+}
---
+++ Web.Controller/Helpers/PagerHelper.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Web.Controller.Helpers
+{
+    public static class ListOfBeverages
+    {
+        public static List<ListBeveragesViewModel> TopAlcoholicDrinksUk()
+        {
+            List<ListBeveragesViewModel> lstBeers = new List<ListBeveragesViewModel>
+            {
+                new ListBeveragesViewModel("Peroni", "beer"),
+                new ListBeveragesViewModel("Guinness", "beer"),
+                new ListBeveragesViewModel("Bulmers", "cider"),
+                new ListBeveragesViewModel("Stella Artois", "beer"),
+                new ListBeveragesViewModel("Magners", "cider"),
+                new ListBeveragesViewModel("Heineken", "beer"),
+                new ListBeveragesViewModel("Amstel", "beer"),
+                new ListBeveragesViewModel("Corona", "beer"),
+                new ListBeveragesViewModel("Becks", "beer"),
+                new ListBeveragesViewModel("Budweiser", "beer"),
+                new ListBeveragesViewModel("Kopparberg", "cider"),
+                new ListBeveragesViewModel("San Miguel", "beer"),
+                new ListBeveragesViewModel("Tetley's Brewery", "beer"),
+                new ListBeveragesViewModel("Kronenburg 1664", "beer"),
+                new ListBeveragesViewModel("Strongbow", "cider"),
+                new ListBeveragesViewModel("Carlsberg", "beer"),
+                new ListBeveragesViewModel("Hobgoblin", "cider"),
+                new ListBeveragesViewModel("Sol", "beer"),
+                new ListBeveragesViewModel("Thatchers Gold", "cider"),
+                new ListBeveragesViewModel("Pepsi Max", "soft drink")
+            };
+
+
+            return lstBeers;
+
+        }
+    }
+
+    public class DisplayPagerViewModel
+    {
+        public IEnumerable<ListBeveragesViewModel> DisplaySearchResults { get; set; }
+        public PagerHelper.Pagination Pager                             { get; set; }
+        public int NumberOfResults                                      { get; set; }
+    }
+
+    public class ListBeveragesViewModel
+    {
+        public ListBeveragesViewModel(string beverage, string type)
+        {
+            Beverage    = beverage;
+            Type        = type;
+        }
+
+        public string Beverage { get; set; }
+        public string Type { get; set; }
+    }
+    public static class PagerHelper
+    {
+
+        public class Pagination
+        {
+            public Pagination(int totalItems, int? page, int pageSize = 10)
+            {
+                var totalPages = (int)Math.Ceiling(totalItems / (decimal)pageSize);
+
+                if (page > totalPages || page < 0)
+                {
+                    page = 1;
+                }
+                var currentPage     = page ?? 1;
+                var startPage       = currentPage - 5;
+                var endPage         = currentPage + 4;
+
+                if (startPage <= 0)
+                {
+                    endPage -= (startPage - 1);
+                    startPage = 1;
+                }
+                if (endPage > totalPages)
+                {
+                    endPage = totalPages;
+                    if (endPage > 10)
+                    {
+                        startPage = endPage - 9;
+                    }
+                }
+
+                StartPage       = startPage;
+                CurrentPage     = currentPage;
+                PageSize        = pageSize;
+                TotalItems      = totalItems;
+                TotalPages      = totalPages;
+                EndPage         = endPage;
+            }
+
+            public int StartPage        { get; }
+            public int CurrentPage      { get; }
+            public int PageSize         { get; }
+            public int TotalItems       { get; }
+            public int TotalPages       { get; }
+            public int EndPage          { get; }
+        }
+    }
+}
