079ba64d30d0e165c7f472fa3473552770535a8a
George <gwphillipson@yahoo.co.uk>
2021-11-25 11:48:45 +0000

    Added Tuple Example

---
+++ Web.Controller/Controllers/TupleExampleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Web.Model.TupleModel;
+
+namespace Web.Controller.Controllers
+{
+    public class TupleExampleController : System.Web.Mvc.Controller
+    {
+        public ActionResult Index(bool hasPet = true)
+        {
+            var tupleValues = DisplayListOfPetsTuple.ReturnTupleExamples(hasPet);
+
+            ViewBag.Name            = tupleValues.Name;
+            ViewBag.HasPet          = tupleValues.HasPet;
+            ViewBag.ListOfPets      = tupleValues.ListOfPets;
+
+            return View();
+        }
+    }
+}
---
+++ Web.Model/TupleModel/DisplayListOfPetsTuple.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Web.Model.TupleModel
+{
+    public static class DisplayListOfPetsTuple
+    {
+        public static (string Name, bool HasPet, List<string> ListOfPets) ReturnTupleExamples(bool havePet)
+        {
+            string name = "George";
+            bool hasPet = havePet;
+
+            List<string> typeOfPets = new List<string>
+            {
+                "Dog",
+                "Cat",
+                "Fish"
+            };
+
+            return (name, hasPet, typeOfPets);
+        }
+    }
+}
