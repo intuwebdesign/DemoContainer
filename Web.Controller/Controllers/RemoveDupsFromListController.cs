e10acd3405f7a209c2b6bc0fe3692c4d50031030
George <gwphillipson@yahoo.co.uk>
2022-01-22 17:22:23 +0000

    Added C# Remove Duplicates from list 22 Jan 2022 17:22

---
+++ Web.Controller/Controllers/RemoveDupsFromListController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Web.Model.RemoveDupsFromList;
+
+namespace Web.Controller.Controllers
+{
+    public class RemoveDupsFromListController : System.Web.Mvc.Controller
+    {
+        public ActionResult Index()
+        {
+            ListOfTestDataNoDups listDataNoDups         = new ListOfTestDataNoDups();
+            ListOfTestDataWithDups listDataWithDups     = new ListOfTestDataWithDups();
+
+            var dataNoDups          = listDataNoDups.ListDataNoDups();
+            var dataWithDups        = listDataWithDups.ListDataWithDups();
+
+            List<ListOfTestData> joinList = dataNoDups.Select(data =>  new ListOfTestData(data.Name, data.Age)).ToList();
+            joinList.AddRange(dataWithDups.Select(data =>    new ListOfTestData(data.Name, data.Age)));
+
+            var listMergedAndDupsRemoved = joinList
+                .GroupBy(x => new
+                {
+                    x.Name,
+                    x.Age
+                })
+                .Select(x => new ListOfTestData
+                {
+                    Name    = x.Key.Name,
+                    Age     = x.Key.Age
+                }).ToList();
+
+            ViewBag.Message = listMergedAndDupsRemoved.OrderBy(x=>x.Name).ThenByDescending(x=>x.Age);
+
+            var model = new DisplayNoDupsFromList
+            {
+                DisplayData = listMergedAndDupsRemoved.OrderBy(x=>x.Name).ThenByDescending(x=>x.Age)
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Web.Model/RemoveDupsFromList/ListOfTestData.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace Web.Model.RemoveDupsFromList
+{
+    public class DisplayNoDupsFromList
+    {
+        public IEnumerable<ListOfTestData> DisplayData { get; set; }
+    }
+    public class ListOfTestData
+    {
+        public ListOfTestData(string name, int age)
+        {
+            Name    = name;
+            Age     = age;
+        }
+
+        public ListOfTestData(){}
+        public string Name      { get; set; }
+        public int Age          { get; set; }
+    }
+
+    public class ListOfTestDataNoDups
+    {
+        public List<ListOfTestData> ListDataNoDups()
+        {
+            List<ListOfTestData> data = new List<ListOfTestData>
+            {
+                new ListOfTestData("Peter", 24),
+                new ListOfTestData("Harry", 30),
+                new ListOfTestData("Luke", 15),
+                new ListOfTestData("Helen", 21),
+                new ListOfTestData("Jemma", 17),
+                new ListOfTestData("George", 50),
+                new ListOfTestData("George", 51)
+
+            };
+            return data;
+        }
+    }
+
+    public class ListOfTestDataWithDups
+    {
+        public List<ListOfTestData> ListDataWithDups()
+        {
+            List<ListOfTestData> data = new List<ListOfTestData>
+            {
+                new ListOfTestData("Peter", 24),
+                new ListOfTestData("Charlie", 33),
+                new ListOfTestData("Graham", 33),
+                new ListOfTestData("George", 50),
+                new ListOfTestData("George", 50),
+                new ListOfTestData("George", 49),
+                new ListOfTestData("Lisa", 33),
+                new ListOfTestData("Mary", 45),
+                new ListOfTestData("Mary", 45)
+            };
+            return data;
+        }
+    }
+}
